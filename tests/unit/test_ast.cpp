8b13a25086ea73827570b69fb9fb996000a3357f
rwiggins1 <wigginsricky94@gmail.com>
2026-01-02 16:37:58 -0600

    test: add test for all LiteralAST's and VariableDeclarationAST

--- tests/unit/test_ast.cpp
+++ tests/unit/test_ast.cpp
@@ -9,3 +9,26 @@ TEST(astTest,  NumberExpr) {
 	ASSERT_NE(expr, nullptr);
 	ASSERT_EQ(expr->getValue(), 42.0);
 }
+
+TEST(astTest, StringLiteral) {
+	auto str_lit = std::make_unique<StringLiteralAST>("Hello, World!");
+	ASSERT_NE(str_lit, nullptr);
+	ASSERT_EQ(str_lit->getValue(), "Hello, World!");
+}
+
+TEST(astTest, CharLiteral) {
+	auto char_lit = std::make_unique<CharLiteralAST>('a');
+	ASSERT_NE(char_lit, nullptr);
+	ASSERT_EQ(char_lit->getValue(), 'a');
+}
+
+TEST(astTest, BoolLiteral) {
+	auto bool_lit = std::make_unique<BoolLiteralAST>(true);
+	ASSERT_NE(bool_lit, nullptr);
+	ASSERT_EQ(bool_lit->getValue(), true);
+}
+
+TEST(astTest, VarDec) {
+	auto expr = std::make_unique<NumberExprAST>(42.0);
+	auto var_dec = std::make_unique<VariableDeclarationAST>("int", "x", std::move(expr));
+}
