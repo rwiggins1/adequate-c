ca857577b48fa0a1ca4f0b4e4953ea63fb8b743f
rwiggins1 <wigginsricky94@gmail.com>
2026-02-08 23:12:34 -0600

    feat: add getters to Block, Return and Break statements

--- src/frontend/ast/stmt.cpp
+++ src/frontend/ast/stmt.cpp
@@ -8,4 +8,6 @@ VariableDeclarationAST::VariableDeclarationAST(std::string type,
 					       std::string name,
 					       std::unique_ptr<ExprAST> init)
 	: type(std::move(type)), name(std::move(name)), initializer(std::move(init)) {}
+
+
 }
