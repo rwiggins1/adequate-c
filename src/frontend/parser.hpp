06a003c3894db0e2c1f87e718bef2d6a137fef94
rwiggins1 <wigginsricky94@gmail.com>
2026-03-28 21:03:13 -0500

    refactor: make parser functions public for testing

--- src/frontend/parser.hpp
+++ src/frontend/parser.hpp
@@ -11,6 +11,34 @@
 
 namespace frontend {
 class Parser {
+public:
+	explicit Parser(Lexer &lex, ErrorReporter &errors);
+
+	std::optional<std::unique_ptr<ast::ExprAST>> parseLiteral();
+	[[nodiscard]] bool unaryOperator() const;
+	[[nodiscard]] bool assignmentOperator() const;
+
+	std::optional<std::unique_ptr<types::Type>> parsePrimitiveType();
+	std::optional<std::unique_ptr<types::Type>> parseType();
+
+	std::vector<std::unique_ptr<ast::ExprAST>> parseArgList();
+	std::vector<std::unique_ptr<ast::ExprAST>>
+	    parseArgListTail(std::unique_ptr<ast::ExprAST>);
+
+	std::optional<std::unique_ptr<ast::ExprAST>> parseExpression();
+
+	std::optional<std::unique_ptr<ast::ExprAST>> parseMultiplicativeExpr();
+
+	std::optional<std::unique_ptr<ast::ExprAST>> parseUnaryExpr();
+	std::optional<std::unique_ptr<ast::ExprAST>> parseVarExpr();
+	std::optional<std::unique_ptr<ast::ExprAST>> parsePostfixExpr();
+	std::optional<std::unique_ptr<ast::ExprAST>>
+	    parsePostfixExprTail(std::unique_ptr<ast::ExprAST>);
+
+	std::optional<std::unique_ptr<ast::ExprAST>> parsePrimaryExpr();
+
+	std::optional<std::unique_ptr<ast::StmtAST>> parseVarDecl();
+
 private:
 	Lexer &lexer;
 	Token current;
@@ -20,33 +48,6 @@ private:
 	bool match(TokenType type);
 	static bool isType(TokenType type);
 	static bool isLiteral(TokenType type);
-	ast::BinaryOp getBinaryOp(TokenType type);
 	bool expect(TokenType type);
-	
-	std::optional<std::unique_ptr<ast::ExprAST>> parseLiteral();
-	[[nodiscard]] bool unaryOperator() const;
-	[[nodiscard]] bool assignmentOperator() const;
-
-	std::optional<std::unique_ptr<types::Type>> parsePrimitiveType();
-	std::optional<std::unique_ptr<types::Type>> parseType();
-
-	std::vector<std::unique_ptr<ast::ExprAST>> parseArgList();
-	std::vector<std::unique_ptr<ast::ExprAST>> parseArgListTail(std::unique_ptr<ast::ExprAST>);
-
-	std::optional<std::unique_ptr<ast::ExprAST>> parseExpression();
-
-	std::optional<std::unique_ptr<ast::ExprAST>> parseMultiplicativeExpr();
-
-	std::optional<std::unique_ptr<ast::ExprAST>> parseUnaryExpr();
-	std::optional<std::unique_ptr<ast::ExprAST>> parseVarExpr();
-	std::optional<std::unique_ptr<ast::ExprAST>> parsePostfixExpr();
-	std::optional<std::unique_ptr<ast::ExprAST>> parsePostfixExprTail(std::unique_ptr<ast::ExprAST>);
-
-	std::optional<std::unique_ptr<ast::ExprAST>> parsePrimaryExpr();
-
-
-public:
-	explicit Parser(Lexer &lex, ErrorReporter &errors);
-	std::optional<std::unique_ptr<ast::StmtAST>> parseVarDecl();
 };
 } // namespace frontend
