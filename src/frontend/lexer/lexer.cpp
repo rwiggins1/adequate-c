40e20936c3c14c7564efad91f7f96bf601f89eb0
rwiggins1 <wigginsricky94@gmail.com>
2026-03-31 02:57:19 -0500

    feat: add character literal support to lexer

--- src/frontend/lexer/lexer.cpp
+++ src/frontend/lexer/lexer.cpp
@@ -5,10 +5,9 @@
 #include <string>
 
 namespace frontend {
-Lexer::Lexer(std::string src )
-	: source(std::move(src)), src_length(source.length()), position(0), line(1), column(1),
-	current(source.empty() ? '\0' : source[0]) {
-} 
+Lexer::Lexer(std::string src)
+    : source(std::move(src)), src_length(source.length()), position(0), line(1),
+      column(1), current(source.empty() ? '\0' : source[0]) {}
 
 void Lexer::advance() noexcept {
 	if (current == '\n') {
@@ -20,21 +19,23 @@ void Lexer::advance() noexcept {
 	}
 
 	position++;
-	current = (position < src_length)  ? source[position] : '\0';
+	current = (position < src_length) ? source[position] : '\0';
 }
 
 char Lexer::peekNext() noexcept {
-	return (position < src_length) ? source[position+1] : '\0';
+	return (position < src_length) ? source[position + 1] : '\0';
 }
 
 void Lexer::skipWhitespace() noexcept {
-	while (position < src_length && isspace(static_cast<unsigned char>(current)) != 0) {
+	while (position < src_length &&
+	       isspace(static_cast<unsigned char>(current)) != 0) {
 		advance();
 	}
 }
 
 void Lexer::skipSingleLineComment() noexcept {
-	advance(); advance();
+	advance();
+	advance();
 
 	while (current != '\n') {
 		advance();
@@ -43,12 +44,15 @@ void Lexer::skipSingleLineComment() noexcept {
 }
 
 void Lexer::skipMultiLineComment() noexcept {
-	advance(); advance();
+	advance();
+	advance();
 
 	while (current != '*' && peekNext() != '/') {
 		advance();
 	}
-	advance(); advance(); advance();
+	advance();
+	advance();
+	advance();
 }
 
 // Determines identifier
@@ -57,14 +61,15 @@ Token Lexer::identifier() {
 	size_t startColumn = column;
 
 	std::string text;
-	text+=current;
+	text += current;
 	advance();
 
-	while(std::isalnum(static_cast<unsigned char>(current)) != 0 || current == '_') {
-		text+=current;
+	while (std::isalnum(static_cast<unsigned char>(current)) != 0 ||
+	       current == '_') {
+		text += current;
 		advance();
 	}
-	
+
 	auto it = KEYWORDS.find(text);
 	if (it != KEYWORDS.end()) {
 		return {it->second, text, startLine, startColumn};
@@ -77,27 +82,62 @@ Token Lexer::number() {
 	size_t startLine = line;
 	size_t startColumn = column;
 
-
 	std::string text;
-	text+= current;
+	text += current;
 	advance();
 
-	while(std::isdigit(static_cast<unsigned char>(current)) != 0) {
-		text+=current;
+	while (std::isdigit(static_cast<unsigned char>(current)) != 0) {
+		text += current;
 		advance();
 	}
 
 	// check for float or double
-	if (current == '.' && std::isdigit(static_cast<unsigned char>(peekNext())) != 0) {
-		text+=current;
+	if (current == '.' &&
+	    std::isdigit(static_cast<unsigned char>(peekNext())) != 0) {
+		text += current;
 		advance();
 		while (std::isdigit(static_cast<unsigned char>(current)) != 0) {
-			text+=current;
+			text += current;
 			advance();
 		}
 	}
 
 	return {TokenType::NUMBER, text, startLine, startColumn};
+}
+
+// Determine char literal
+Token Lexer::char_lit() {
+	size_t startLine = line;
+	size_t startColumn = column;
+
+	std::string text;
+	text += current; // Opening '
+	advance();
+
+	// Regular character
+	if (current != '\'' && position < src_length) {
+		text += current;
+		advance();
+	}
+
+	// Check for more than one character
+	if (current != '\'') {
+		while (current != '\'' && position < src_length) {
+			text += current;
+			advance();
+		}
+		if (current == '\'') {
+			text += current;
+			advance();
+		}
+
+		return {TokenType::INVALID, text, startLine, startColumn};
+	}
+
+	text += current; // Closing '
+	advance();
+
+	return {TokenType::CHAR_LIT, text, startLine, startColumn};
 }
 
 // Determine string literal
@@ -110,16 +150,16 @@ Token Lexer::string_lit() {
 	advance();
 
 	while (current != '"' && position < src_length) {
-		text+=current;
+		text += current;
 		advance();
 	}
-	text+=current; // add closing qoutes
+	text += current; // add closing qoutes
 	advance();
 	return {TokenType::STRING_LIT, text, startLine, startColumn};
 }
 
 // Constructs token
-Token Lexer::makeToken(TokenType type, const std::string& lexme) {
+Token Lexer::makeToken(TokenType type, const std::string &lexme) {
 	Token token(type, lexme, line, column);
 	advance();
 	return token;
@@ -127,16 +167,16 @@ Token Lexer::makeToken(TokenType type, const std::string& lexme) {
 
 // Tokenizer
 Token Lexer::tokenize() {
-	//EOF
-	if (position >= src_length){
+	// EOF
+	if (position >= src_length) {
 		return {TokenType::T_EOF, "\0", line, column};
 	}
-	
+
 	skipWhitespace();
 
 	if (current == '/' && peekNext() == '/') {
 		Lexer::skipSingleLineComment();
-	} 
+	}
 	else if (current == '/' && peekNext() == '*') {
 		Lexer::skipMultiLineComment();
 	}
@@ -149,152 +189,155 @@ Token Lexer::tokenize() {
 		return number();
 	}
 
+	if (current == '\'') {
+		return char_lit();
+	}
+
 	if (current == '"') {
 		return string_lit();
 	}
 
 	switch (current) {
-		// Operators
-		case '+':
-			advance();
-			if (current == '+') {
-				return makeToken(TokenType::PLUS_PLUS, "++");
-			}
-			if (current == '=') {
-				return makeToken(TokenType::PLUS_ASSIGN, "+=");
-			}
-			return {TokenType::PLUS, "+", line, column};
-		case '-':
-			advance();
-			if (current == '-') {
-				return makeToken(TokenType::MINUS_MINUS, "--");
-			}
-			if (current == '=') {
-				return makeToken(TokenType::MINUS_ASSIGN, "-=");
-			}
-			if (current == '>') {
-				return makeToken(TokenType::ARROW,"->");
-			}	
-			return {TokenType::MINUS, "-", line, column};
-		case '*':
-			advance();
-			if (current == '=') {
-				return makeToken(TokenType::MULTIPLY_ASSIGN, "*=");
-			}
-			return {TokenType::MULTIPLY, "*", line, column};
-		case '/':
-			advance();
-			if (current == '=') {
-				return makeToken(TokenType::DIVIDE_ASSIGN, "/=");
-			}
-			return {TokenType::DIVIDE, "/", line, column};
-		case '%':
-			advance();
-			if (current == '=') {
-				return makeToken(TokenType::MODULO_ASSIGN, "%=");
-			}
-			return {TokenType::MODULO, "%", line, column};
-		case '=':
-			advance();
-			if (current == '=') {
-				return makeToken(TokenType::EQUAL, "==");
-			}
-			return {TokenType::ASSIGN, "=", line, column};
-		case '!':
-			advance();
-			if (current == '=') {
-				return makeToken(TokenType::NOT_EQUAL, "!=");
-			}
-			return {TokenType::NOT, "!", line, column};
-		case '>':
-			advance();
-			if (current == '=') {
-				return makeToken(TokenType::GREATER_EQUAL, ">=");
-			}
-			if (current == '>') {
-				advance();
-				if (current == '=') {
-					return makeToken(TokenType::RIGHT_SHIFT_ASSIGN, ">>=");
-				}
-				return {TokenType::RIGHT_SHIFT, ">>", line, column};
-			}
-			return {TokenType::GREATER, ">", line, column};
-		case '<':
+	// Operators
+	case '+':
+		advance();
+		if (current == '+') {
+			return makeToken(TokenType::PLUS_PLUS, "++");
+		}
+		if (current == '=') {
+			return makeToken(TokenType::PLUS_ASSIGN, "+=");
+		}
+		return {TokenType::PLUS, "+", line, column};
+	case '-':
+		advance();
+		if (current == '-') {
+			return makeToken(TokenType::MINUS_MINUS, "--");
+		}
+		if (current == '=') {
+			return makeToken(TokenType::MINUS_ASSIGN, "-=");
+		}
+		if (current == '>') {
+			return makeToken(TokenType::ARROW, "->");
+		}
+		return {TokenType::MINUS, "-", line, column};
+	case '*':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::MULTIPLY_ASSIGN, "*=");
+		}
+		return {TokenType::MULTIPLY, "*", line, column};
+	case '/':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::DIVIDE_ASSIGN, "/=");
+		}
+		return {TokenType::DIVIDE, "/", line, column};
+	case '%':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::MODULO_ASSIGN, "%=");
+		}
+		return {TokenType::MODULO, "%", line, column};
+	case '=':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::EQUAL, "==");
+		}
+		return {TokenType::ASSIGN, "=", line, column};
+	case '!':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::NOT_EQUAL, "!=");
+		}
+		return {TokenType::NOT, "!", line, column};
+	case '>':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::GREATER_EQUAL, ">=");
+		}
+		if (current == '>') {
 			advance();
 			if (current == '=') {
-				return makeToken(TokenType::LESS_EQUAL, "<");
+				return makeToken(TokenType::RIGHT_SHIFT_ASSIGN,
+						 ">>=");
 			}
-			if (current == '<') {
-				advance();
-				if (current == '=') {
-					return makeToken(TokenType::LEFT_SHIFT_ASSIGN, "<<=");
-				}
-				return {TokenType::LEFT_SHIFT, "<<", line, column};
-			}
-			return {TokenType::LESS, "<", line, column};
-		case '&':
-			advance();
-			if (current == '&') {
-				return makeToken(TokenType::AND, "&&");
-			}
-			if (current == '=') {
-				return makeToken(TokenType::BIT_AND_ASSIGN, "&=");
-			}
-			return {TokenType::BIT_AND, "&", line, column};
-		case '|':
-			advance();
-			if (current == '|') {
-				return makeToken(TokenType::OR, "||");
-			}
-			if (current == '=') {
-				return makeToken(TokenType::BIT_OR_ASSIGN, "|=");
-			}
-			return {TokenType::BIT_OR, "|", line, column};
-		case '^':
+			return {TokenType::RIGHT_SHIFT, ">>", line, column};
+		}
+		return {TokenType::GREATER, ">", line, column};
+	case '<':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::LESS_EQUAL, "<");
+		}
+		if (current == '<') {
 			advance();
 			if (current == '=') {
-				return makeToken(TokenType::BIT_XOR_ASSIGN, "^=");
+				return makeToken(TokenType::LEFT_SHIFT_ASSIGN,
+						 "<<=");
 			}
-			return {TokenType::BIT_XOR, "^", line, column};
-		case '~':
-			return makeToken(TokenType::BIT_NOT, "~");
-		case '@':
-			return makeToken(TokenType::REFERENCE, "@");
-		case '.':
-			return makeToken(TokenType::DOT, ".");
-		case '?':
-			return makeToken(TokenType::QUESTION, "?");
-		// Delimiters
-		case ':':
-			advance();
-			if (current == ':') {
-				return makeToken(TokenType::SCOPE, "::");
-			}
-			return {TokenType::COLON, ":", line, column};
-		case ';':
-			return makeToken(TokenType::SEMICOLON, ";");
-		case ',':
-			return makeToken(TokenType::COMMA, ",");
-		case '[':
-			return makeToken(TokenType::LSQRBRACKET, "[");
-		case ']':
-			return makeToken(TokenType::RSQRBRACKET, "]");
-		case '(':
-			return makeToken(TokenType::OPAREN, "(");
-		case ')':
-			return makeToken(TokenType::CPAREN, ")");
-		case '{':
-			return makeToken(TokenType::LBRACE, "{");
-		case '}':
-			return makeToken(TokenType::RBRACE, "}");
-		default:	
-			return makeToken(TokenType::INVALID, std::string(1, current));
+			return {TokenType::LEFT_SHIFT, "<<", line, column};
+		}
+		return {TokenType::LESS, "<", line, column};
+	case '&':
+		advance();
+		if (current == '&') {
+			return makeToken(TokenType::AND, "&&");
+		}
+		if (current == '=') {
+			return makeToken(TokenType::BIT_AND_ASSIGN, "&=");
+		}
+		return {TokenType::BIT_AND, "&", line, column};
+	case '|':
+		advance();
+		if (current == '|') {
+			return makeToken(TokenType::OR, "||");
+		}
+		if (current == '=') {
+			return makeToken(TokenType::BIT_OR_ASSIGN, "|=");
+		}
+		return {TokenType::BIT_OR, "|", line, column};
+	case '^':
+		advance();
+		if (current == '=') {
+			return makeToken(TokenType::BIT_XOR_ASSIGN, "^=");
+		}
+		return {TokenType::BIT_XOR, "^", line, column};
+	case '~':
+		return makeToken(TokenType::BIT_NOT, "~");
+	case '@':
+		return makeToken(TokenType::REFERENCE, "@");
+	case '.':
+		return makeToken(TokenType::DOT, ".");
+	case '?':
+		return makeToken(TokenType::QUESTION, "?");
+	// Delimiters
+	case ':':
+		advance();
+		if (current == ':') {
+			return makeToken(TokenType::SCOPE, "::");
+		}
+		return {TokenType::COLON, ":", line, column};
+	case ';':
+		return makeToken(TokenType::SEMICOLON, ";");
+	case ',':
+		return makeToken(TokenType::COMMA, ",");
+	case '[':
+		return makeToken(TokenType::LSQRBRACKET, "[");
+	case ']':
+		return makeToken(TokenType::RSQRBRACKET, "]");
+	case '(':
+		return makeToken(TokenType::OPAREN, "(");
+	case ')':
+		return makeToken(TokenType::CPAREN, ")");
+	case '{':
+		return makeToken(TokenType::LBRACE, "{");
+	case '}':
+		return makeToken(TokenType::RBRACE, "}");
+	default:
+		return makeToken(TokenType::INVALID, std::string(1, current));
 	}
 }
 
-Token Lexer::get() {
-	return tokenize();
-}
+Token Lexer::get() { return tokenize(); }
 
-}
-
+} // namespace frontend
